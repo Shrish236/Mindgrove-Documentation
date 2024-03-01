8eade1a621d9c6cda5d18ca11bff9bfa0d98f89d
Shrish236 <shrishrajamohan@gmail.com>
2024-03-01 14:08:25 +0530

    Updated Code

--- Plotly Dash/app.py
+++ Plotly Dash/app.py
@@ -1,8 +1,10 @@
 # Import packages
+import dash
 from flask import Flask, request
 from dash import Dash, html, dash_table, dcc, callback, Output, Input, State
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 import plotly.figure_factory as ff
 import dash_bootstrap_components as dbc
 import json, base64, io
@@ -11,7 +13,10 @@ import csv
 
 table = ""
 data_format = ""
+x_axis_options = []
+y_axis_options = []
 graph_list = []
+df = pd.DataFrame()
 # Incorporate data
 # df = pd.read_csv('static/Employee.csv')
 
@@ -34,11 +39,11 @@ app = Dash(__name__, server=server, external_stylesheets=[dbc.themes.BOOTSTRAP, 
 # ])
 
 items = [
-    dbc.DropdownMenuItem("JSON Format", id="json_format"),
-    dbc.DropdownMenuItem(divider=True),
+    # dbc.DropdownMenuItem("JSON Format", id="json_format"),
+    # dbc.DropdownMenuItem(divider=True),
     dbc.DropdownMenuItem("Local CSV File", id="csv_local"),
-    dbc.DropdownMenuItem(divider=True),
-    dbc.DropdownMenuItem("Link to CSV File", id="csv_remote"),
+    # dbc.DropdownMenuItem(divider=True),
+    # dbc.DropdownMenuItem("Link to CSV File", id="csv_remote"),
 ]
 
 graph_data_json = html.Div([
@@ -91,7 +96,7 @@ graph_data_local = html.Div([
         "Only .csv files are supported!",
         color="danger"
     ),
-], id='1')
+], id='1', className='p-1')
 
 graph_data_remote = html.Div([
     html.P(
@@ -120,7 +125,7 @@ home_page_layout = html.Div([
                     html.Div([
                         dbc.Label("Select your data format", html_for="example-email"),
                         dbc.DropdownMenu(
-                            label="Data Format", children=items
+                            label="Data Format", children=items, id='format-dropdown'
                         ),
                         dbc.FormText(
                             "Appropriate data format must be chosen to plot graphs accordingly",
@@ -186,9 +191,9 @@ home_page_layout = html.Div([
                                         # dbc.Input(placeholder="Eg: https://www.drive.google.com/file", type="text"),
                                         # dbc.FormText("Make sure the file is given open access!", className='text-danger'),
                                     # ], 
-                                    id='data-format'),
+                                    id='data-format', className='p-1'),
                                     html.Div([
-                                        dbc.Button("Submit", outline=True, color="primary", id='submit-button', href='/graph-page'),
+                                        dbc.Button("Upload Data", outline=True, color="primary", id='submit-button', ),
                                     ], className='d-flex justify-content-center p-2 pt-4'),
                                     
                                 ]
@@ -205,10 +210,14 @@ home_page_layout = html.Div([
                 html.P(id='dummy-tag'),
                 html.P(id='dummy-tag1'),
             ], className='col px-4')
-        ], className='row')
+        ], className='row'),
+        html.Div([
+        ], className='row', id='columns-select'),
     ], className='container min-vh-75 bg-light rounded-1')
 ], className='d-flex bg-secondary min-vh-100 min-vw-100 px-3 py-5 justify-content-center')
-selection_state = [0, 0, 0]
+
+
+# selection_state = [0, 0, 0]
 # Add controls to build the interaction
 # @callback(
 #         Output(component_id='controls-and-graph', component_property='figure'),
@@ -220,14 +229,14 @@ selection_state = [0, 0, 0]
 # )
 # app.layout = home_page_layout
 @callback(
-        Output('data-format', 'children'), 
+        Output('data-format', 'children'),
         [
-            Input(component_id='json_format', component_property='n_clicks'), 
+            # Input(component_id='json_format', component_property='n_clicks'), 
             Input('csv_local', 'n_clicks'), 
-            Input('csv_remote', 'n_clicks')
+            # Input('csv_remote', 'n_clicks')
         ]
 )
-def update_graph(json, csv_local, csv_remote):
+def update_graph(csv_local):
     # print(x, y)
     # if g == 'Bar':
     #     fig = px.bar(df, x=x, y=y)
@@ -239,19 +248,18 @@ def update_graph(json, csv_local, csv_remote):
     # if g == 'Scatter':
     #     fig = px.scatter(df, x=x, y=y)
     global data_format
-    if(json!=None and json > selection_state[0]):
-        selection_state[0] = json
-        data_format = "json"
-        return graph_data_json
-    if(csv_local!=None and csv_local > selection_state[1]):
-        selection_state[1] = csv_local
+    changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
+    # if('json_format' in  changed_id):
+    #     data_format = "json"
+    #     return graph_data_json
+    if('csv_local' in changed_id):
         data_format = "local"
         return graph_data_local
-    if(csv_remote!=None and csv_remote > selection_state[2]):
-        selection_state[2] = csv_remote
-        data_format = "remote"
-        return graph_data_remote
+    # elif('csv_remote' in changed_id):
+    #     data_format = "remote"
+    #     return graph_data_remote
     return None
+
 
 
 @callback( 
@@ -300,27 +308,8 @@ def update_graph_list(list1, list2):
 
 
 
-def parse_contents(contents, filename, date):
-    content_type, content_string = contents.split(',')
-
-    # define data frame as global
-    decoded = base64.b64decode(content_string)
-    try:
-        if 'csv' in filename:
-            # Assume that the user uploaded a CSV file
-            df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
-            df.to_csv('static/file.csv', index=False)
-
-        elif 'xls' in filename:
-            # Assume that the user uploaded an Excel file
-            df = pd.read_excel(io.BytesIO(decoded))
-
-    except Exception as e:
-        print(e)
-        return html.Div([
-            'There was an error processing this file.'
-        ])
-
+# def parse_contents(contents, filename, date):
+    
 @callback( 
         Output('dummy-tag', 'value'),
         Input('submit-button', 'n_clicks'), 
@@ -349,8 +338,60 @@ def get_json_data(submit, json_input):
 
     return None
 
+
+local_file_uploaded = html.Div([
+    "File uploaded successfully!",
+], className="text-center font-italic")
+file_not_uploaded=html.Div([
+    'Drag and Drop or ',
+    html.A('Select Files', className='fst-italic font-decoration-underline')
+])
+
+column_select_layout = html.Div([
+    html.Hr(),
+    html.Div([
+        dbc.Card(
+            [
+                dbc.CardHeader("Column selection for graph representation"),
+                dbc.CardBody(
+                    [
+                        html.H5("Select your x-axis and y-axis columns", className="card-title"),
+                        html.Div([
+                            html.Div([
+                                html.P("X-axis", className="font-italic"),
+                                dcc.Dropdown(
+                                    multi=True, id='x-axis-select'
+                                )
+                            ], className='col text-center p-2 mr-2'),
+                            html.Div([
+                                html.P("Y-axis", className="font-italic"),
+                                dcc.Dropdown(
+                                    multi=True, id='y-axis-select'
+                                )
+                            ], className='col text-center p-2'),
+                        ],id='k', className='d-flex justify-content-between p-3', style={
+                            'justify-content':'space-between'
+                        }),
+                        html.Div([
+                            dbc.Button("Submit", outline=True, color="primary", id='columns-submit', href='/graph-page'),
+                        ], className='d-flex justify-content-center p-2 pt-4'),
+                        html.P(id='dummy-tag-column-1')
+                                    
+                    ]
+                ),
+            ],
+            color="primary", 
+            outline=True,
+            style={
+                'height':'auto',
+                'width' : '100%',
+            }
+        )
+    ], className='d-flex align-items-center'),
+], className="px-5 py-3 text-center")
 @callback(
-    Output('dummy-tag1', 'value'),
+    Output('upload-local-csv', 'children'),
+    Output('columns-select', 'children'),
     Input('submit-button', 'n_clicks'),
     State('upload-local-csv', 'contents'),
     State('upload-local-csv', 'filename'),
@@ -360,8 +401,60 @@ def get_local_csv(submit, contents, filename, last_modified):
     if(submit and data_format=="local"):
         # print("hello")
         if filename is not None:
-            children = parse_contents(contents, filename, last_modified)
-            return f'{filename} is Uploaded Successfully...'
+            # children = parse_contents(contents, filename, last_modified)
+            content_type, content_string = contents.split(',')
+            # define data frame as global
+            decoded = base64.b64decode(content_string)
+            global df
+            try:
+                if 'csv' in filename:
+                    
+                    # Assume that the user uploaded a CSV file
+                    df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
+                    # app.session['data'] = df
+                    # print(df)
+                    df.to_csv('static/file.csv', index=False)
+
+                elif 'xls' in filename:
+                    
+                    # Assume that the user uploaded an Excel file
+                    df = pd.read_excel(io.BytesIO(decoded))
+                
+                return local_file_uploaded, column_select_layout
+
+            except Exception as e:
+                print(e)
+                return html.Div([
+                    'There was an error processing this file.'
+                ]), None, 
+
+            
+    return file_not_uploaded, None
+
+
+
+@callback(
+    Output('x-axis-select', 'options'),
+    Output('y-axis-select', 'options'),
+    Input('submit-button', 'n_clicks')
+)
+def update_column_select(submit):
+    global df
+    if(submit):
+        return df.columns, df.columns
+
+@callback(
+        Output('dummy-tag-column-1', 'value'),
+        Input('x-axis-select', 'value'),
+        Input('y-axis-select', 'value'),
+        Input('columns-select', 'n_clicks')
+)
+def set_columns(xaxis, yaxis, submit):
+    global x_axis_options
+    global y_axis_options
+    if(submit):
+        x_axis_options = xaxis
+        y_axis_options = yaxis
     return None
 
 @callback(
@@ -372,6 +465,7 @@ def get_local_csv(submit, contents, filename, last_modified):
 def get_remote_csv(submit, file):
     if(submit and data_format=="remote"):
         # print("hello")
+        global df
         df = pd.read_csv(file)
         df.to_csv('static/file.csv', index=False)
     return None
@@ -381,30 +475,12 @@ app.layout = html.Div([
     html.Div(id='page-content'),
 ])
 
-items2 = [
-    dbc.DropdownMenuItem("Experiment 1", id="json_format"),
-    dbc.DropdownMenuItem(divider=True),
-    dbc.DropdownMenuItem("Experiment 2", id="csv_local"),
-    dbc.DropdownMenuItem(divider=True),
-    dbc.DropdownMenuItem("Experiment", id="csv_remote"),
-]
-
 graph_page = html.Div([
     html.Div([
         html.Div([
             html.P('Graphical Representation', className='fs-3 fw-medium text-start text-primary col', style={
                 'font-family': 'Arial',
             }),
-            html.Div([
-                dbc.DropdownMenu(
-                    label="Experiment", children=items2, className='float-end',style={
-                        'width': 'auto',
-                        'justify-items':'end',
-                        'justify-content':'flex-end',
-                        'justify-self':'right'
-                    }
-                ),
-            ], className='col justify-content-end'),
             html.Div([
                 html.Div([
                     html.Div([
@@ -438,11 +514,26 @@ graph_page = html.Div([
                     'padding-left': 5,
                     'margin-top':10
                 }, id="graph-type"),
+                html.Div(
+                    [
+                        html.H6("Graph scale:"),
+                        dbc.Checklist(
+                            options=[
+                                {"label": "Log scale", "value": 1},
+                            ],
+                            value=[1],
+                            id="log-scale-switch",
+                            switch=True,
+                        ),
+                    ],
+                className='my-2'),
             ], className='d-flex flex-column'),
             html.Div([
                 dcc.Graph(figure={}, id='graph')
             ], className='col ps-3', style={
                 'border-left': '1px solid #7A7B7C',
+                'width': 'inherit',
+                'overflow-x':'auto'
             }),
         ],className='d-flex p-4', style={
             'gap':40
@@ -467,12 +558,12 @@ def display_page(pathname):
     ])
 def display_graph(pathname):
     if(pathname == '/graph-page'):
-        table = pd.read_csv("static/file.csv")
-        columns = table.columns
-        selected_column_x = columns[0]
-        selected_column_y = columns[1]
+        global df
+        y_axis_options.insert(0, 'All')
+        selected_column_x = x_axis_options[0]
+        selected_column_y = y_axis_options[0]
         # fig = px.line(table, x=selected_column_x, y=selected_column_y, markers=True)
-        return columns, selected_column_x, columns, selected_column_y, graph_list[0]
+        return x_axis_options, selected_column_x, y_axis_options, selected_column_y, graph_list[0]
     return None, None, None, None
 
 @callback(
@@ -481,24 +572,69 @@ def display_graph(pathname):
         Input(component_id='graph-type', component_property='value'),
         Input('x-axis-radio', 'value'),
         Input('y-axis-radio', 'value'),
+        Input('log-scale-switch', 'value')
     ]
 )
-def change_graph_type(g, x, y):
+def change_graph_type(g, x, y, scale):
     table = pd.read_csv("static/file.csv")
+    fig = go.Figure()
+    global df
+    global y_axis_options
+    print(scale)
+    # print(y_axis_options)
+    if(y == 'All'):
+        y = y_axis_options[1:]   
     if g == 'Bar':
-        fig = px.bar(table, x=x, y=y)
+        fig = px.bar(df, x=x, y=y, barmode='group')
     if g == 'Line':
-        df1 = table.sort_values(by=y)
+        df1 = df.sort_values(by=y)
         fig = px.line(df1, x=x, y=y, markers=True)
     if g == 'Histogram':
-        fig = px.histogram(table, x=x, y=y, histfunc='avg')
+        fig = px.histogram(df, x=x, y=y, histfunc='avg')
     if g == 'Scatter':
-        fig = px.scatter(table, x=x, y=y)
+        fig = px.scatter(df, x=x, y=y)
     if g == 'Pie':
-        fig = px.pie(table, values=y, names=x, hole=.2)
+        fig = px.pie(df, values=y, names=x, hole=.2)
     if g == 'Distplot':
         # print(table[y])
-        fig = ff.create_distplot([table[y].values.tolist()], ["data"])
+        fig = ff.create_distplot([df[y].values.tolist()], ["data"])
+
+    if(len(scale)!=0 and scale[0] == 1):
+        fig.update_yaxes(type='log')
+    elif(len(scale)==0):
+        fig.update_yaxes(type='-')
+    fig.update_layout(
+        hovermode="x", 
+        width=2500, 
+        legend=dict(x=0, y=1.1, xanchor='left', 
+        yanchor='top', 
+        orientation='h', 
+        title='Columns', 
+        title_font_family="Times New Roman",
+        font=dict(
+            family="Courier",
+            size=12,
+            color="black",
+        ),
+        ))
+    fig.update_layout(xaxis=dict(
+        title='<b>Iterations</b>',
+        titlefont=dict(
+            family='Arial',
+            size=14,
+            color='black'
+        )
+    ))
+
+    fig.update_layout(yaxis=dict(
+        title='<b>Values</b>',
+        titlefont=dict(
+            family='Arial',
+            size=14,
+            color='black'
+        )
+    ))
+    fig.for_each_trace(lambda t: t.update(name = '<b>' + t.name +'</b>'))
     return fig
 
 
